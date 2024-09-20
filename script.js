3f15bc51d4a3b36e0e9c4911fb8d126504c18f38
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-20 19:28:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function manupulateData(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve([1, 2, 3, 4]);
+		}, 1000);
+	})
+}
+
+manupulateData()
+	.then((data) => {
+		let evenNumbers = data.filter(num => num%2 === 0);
+		document.getElementById("output").innerText = `Even numbers: ${evenNumbers}`;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(evenNumbers);
+			}, 2000);
+		});
+	})
+	.then((evenNumbers) => {
+		let multipliedEvenNumbers = evenNumbers.map(num => num * 2);
+		document.getElementById("output").textContent = `Multiplied even numbers: ${multipliedEvenNumbers}`
+	});
